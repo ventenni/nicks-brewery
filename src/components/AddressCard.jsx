a3f31d3af61f39ad39626aab70d1dc0a19029458
ventenni <ventenni@gmail.com>
2022-03-15 12:48:18 -0600

    Tweaks to responsive styling

--- src/components/AddressCard.jsx
+++ src/components/AddressCard.jsx
@@ -1,26 +1,15 @@
 import { ReactComponent as AddressIcon } from './../assets/icons/location.svg';
 
-const AddressCard = ({
-	street,
-	address2,
-	address3,
-	city,
-	state,
-	postcode,
-	country,
-	province,
-}) => {
+const AddressCard = ({ street, city, state, postcode, country, province }) => {
 	return (
 		<div className="address-card card">
 			<div className="card__body">
 				<h3>Address</h3>
 				<AddressIcon />
 			</div>
-			<div>
-				<h4>{`${street}, ${city} ${state}`}</h4>
-				<p>
-					{province && `${province} `} {`${postcode}`}
-				</p>
+			<div className="address">
+				<p className="address__street">{`${street}, ${city} ${state}, ${postcode}`}</p>
+				<p>{province && `${province} `}</p>
 				<p>{`${country}`}</p>
 			</div>
 		</div>
